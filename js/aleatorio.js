173099d0f3ab28e304910c935aec5e9bcb244071
adriellyvic <149589107+adriellyvic@users.noreply.github.com>
2025-07-30 11:14:06 -0300

    1

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,4 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const nomes = ["Maura", "Lucas", "Pérola", "Nicolas", "João", "Victória", "Gabriel"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
