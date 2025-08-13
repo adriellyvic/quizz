46766f66ca7c6fd8f0b07cd19307f83c1ad8716d
adriellyvic <149589107+adriellyvic@users.noreply.github.com>
2025-08-13 19:03:07 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,8 +1,9 @@
-const nomes = ["Maura", "Lucas", "Pérola", "Nicolas", "João", "Victória", "Gabriel"];
-
-export function aleatorio (lista){
-    const posicao = Math.floor(Math.random()* lista.length);
-    return lista[posicao];
+// Função para embaralhar um array
+export function embaralhar(array) {
+    let copia = [...array];
+    for (let i = copia.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [copia[i], copia[j]] = [copia[j], copia[i]];
+    }
+    return copia;
 }
-
-export const nome = aleatorio(nomes)
